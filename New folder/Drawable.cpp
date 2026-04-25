bdbfb88930062a4b6594a8e7f0be4c41b5b37078
biely-jr <s-mustafa.qarmouti@zewailcity.edu.eg>
2026-04-25 15:02:55 +0300

    Delete New folder directory

--- New folder/Drawable.cpp
+++
@@ -1,9 +0,0 @@
-#include "Drawable.h"
-
-Drawable::Drawable(Game* r_pGame, point r_point, int r_width, int r_height)
-{
-	pGame = r_pGame;
-	RefPoint = r_point;
-	width = r_width;
-	height = r_height;
-}
