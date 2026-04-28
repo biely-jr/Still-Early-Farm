0d2bc5fb0de866978573ff0105ba77954124b1f2
3abdelr7man288 <3abdelr7man15@gmail.com>
2026-04-28 18:05:59 +0300

    Update Animal.h

--- Entities/Animal.h
+++ Entities/Animal.h
@@ -1,16 +1,24 @@
 #pragma once
 #include "../Core/Drawable.h"
+#include <string>
 
 class Animal :public Drawable
 {
 private:
 	string image_path;
+	int productionElapsedSeconds;
+	int productionIntervalSeconds;
+	string productLabel;
 public:
 	point curr_pos;
 	point curr_vel;
-	Animal(Game* r_pGame, point r_point, int r_width, int r_height, string img_path);
+	Animal(Game* r_pGame, point r_point, int r_width, int r_height, string img_path, int productIntervalSeconds, const string& productName);
 	virtual void draw() const override;
 	virtual void moveStep() = 0;   //The action that should be taken each time step
+	void advanceProduction(int elapsedSeconds);
+	int getProductionCounter() const;
+	int getProductionInterval() const;
+	string getProductLabel() const;
 };
 
 class Chick : public Animal
