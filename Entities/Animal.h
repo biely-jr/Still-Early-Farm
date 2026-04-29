7044fc63b20a8d08afb2e69950437c0b2c56ab6c
Kareem Mohamed <s-kareem.fathy@zewailcity.edu.eg>
2026-04-29 14:50:44 +0300

    Update Animal.h

--- Entities/Animal.h
+++ Entities/Animal.h
@@ -13,7 +13,7 @@ public:
 	point curr_pos;
 	point curr_vel;
 	Animal(Game* r_pGame, point r_point, int r_width, int r_height, string img_path, int productIntervalSeconds, const string& productName);
-	virtual void draw() const override;
+	virtual void draw() const override; //The function prototype in Drawable.h is overridden here
 	virtual void moveStep() = 0;   //The action that should be taken each time step
 	void advanceProduction(int elapsedSeconds);
 	int getProductionCounter() const;
@@ -34,3 +34,6 @@ public:
 	Cow(Game* r_pGame, point r_point, int r_width, int r_height, string img_path);
 	virtual void moveStep();
 };
+
+// Notes:
+// virtual : The decision of which function to execute is made at runtime based on the actual (formal) parameters
