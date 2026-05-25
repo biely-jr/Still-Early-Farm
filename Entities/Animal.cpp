4cb8343ae64a8257689166c722b0588fc17a74aa
Abdelrhman Ahmed Shawky <abdelrhman28807@gmail.com>
2026-05-25 09:56:32 +0300

    Update Animal.cpp

--- Entities/Animal.cpp
+++ Entities/Animal.cpp
@@ -1,7 +1,6 @@
 #include "Animal.h"
 #include "../Config/GameConfig.h"
 #include "../Core/Game.h"
-#include <iostream>
 #include <utility>
 #include <cstdlib>
 using namespace std;
@@ -42,9 +41,6 @@ namespace
 			velocity.y = (velocity.y == 0) ? -1 : -velocity.y;
 		}
 
-		// -------------------------------------------------------
-		// Warehouse boundary: bounce the animal off the warehouse
-		// -------------------------------------------------------
 		bool overlapX = (position.x + width > WAREHOUSE_X) && (position.x < WAREHOUSE_X + WAREHOUSE_W);
 		bool overlapY = (position.y + height > WAREHOUSE_Y) && (position.y < WAREHOUSE_Y + WAREHOUSE_H);
 
@@ -60,14 +56,12 @@ namespace
 
 			if (minPenX < minPenY)
 			{
-				// Push out horizontally
 				if (penLeft < penRight) position.x = WAREHOUSE_X - width;
 				else                    position.x = WAREHOUSE_X + WAREHOUSE_W;
 				velocity.x = (velocity.x == 0) ? 1 : -velocity.x;
 			}
 			else
 			{
-				// Push out vertically
 				if (penTop < penBottom) position.y = WAREHOUSE_Y - height;
 				else                    position.y = WAREHOUSE_Y + WAREHOUSE_H;
 				velocity.y = (velocity.y == 0) ? 1 : -velocity.y;
@@ -89,42 +83,33 @@ Animal::Animal(Game* r_pGame, point r_point, int r_width, int r_height, string i
 	productLabel(productName)
 {
 	image_path = img_path;
-	curr_pos = r_point;
 	curr_vel.x = 1;
 	curr_vel.y = 1;
-
 }
 
 void Animal::draw() const
 {
-	//draw image of this object
-	//Both the chick and cow inherit the properties from this function in order to be drawn on the screen
 	window* pWind = pGame->getWind();
-	pWind->DrawImage(image_path, RefPoint.x, RefPoint.y, width, height); // Passing the animal's unique properties as parameters
+	pWind->DrawImage(image_path, RefPoint.x, RefPoint.y, width, height);
 
 	pWind->SetPen(BLACK, 2);
 	pWind->SetBrush(WHITE);
 	pWind->DrawRectangle(RefPoint.x - 6, RefPoint.y - 28, RefPoint.x + 56, RefPoint.y - 4, FILLED, 10, 10);
 	pWind->SetFont(14, BOLD, BY_NAME, "Arial");
-	//Draws a small white rectangle above it and prints its production time
 	pWind->DrawString(RefPoint.x + 4, RefPoint.y - 24, to_string(productionElapsedSeconds) + "/" + to_string(productionIntervalSeconds));
 }
 
 bool Animal::advanceProduction(int elapsedSeconds)
 {
-	// 1. Increase the timer
 	productionElapsedSeconds += elapsedSeconds;
 
-	// 2. Check if the limit is reached
 	if (productionElapsedSeconds >= productionIntervalSeconds)
 	{
-		// Reset the timer for the next product
 		productionElapsedSeconds = 0;
-
-		return true; // Product is ready!
+		return true;
 	}
 
-	return false; // Not ready yet
+	return false;
 }
 
 void Animal::addProductionTime(int seconds)
@@ -153,9 +138,6 @@ Chick::Chick(Game* r_pGame, point r_point, int r_width, int r_height, string img
 
 void Chick::moveStep()
 {
-	cout << "Icon Chick Clicked" << endl;
-
-	// Update position based on velocity
 	RefPoint.x += curr_vel.x * 3;
 	RefPoint.y += curr_vel.y * 3;
 
@@ -168,16 +150,12 @@ Cow::Cow(Game* r_pGame, point r_point, int r_width, int r_height, string img_pat
 
 void Cow::moveStep()
 {
-	cout << "Icon Cow Clicked" << endl;
-
-	// Cows change direction slightly less often than chicks
 	if (rand() % 100 < 5) {
 		curr_vel.x = (rand() % 3) - 1;
 		curr_vel.y = (rand() % 3) - 1;
 		avoidStillVelocity(curr_vel);
 	}
 
-	// Update position based on velocity
 	RefPoint.x += curr_vel.x * 2;
 	RefPoint.y += curr_vel.y * 2;
 
