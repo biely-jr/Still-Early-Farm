8a84995b215899cb3fbb8f6cbad5ead45c75d0f6
Abdelrhman Ahmed Shawky <abdelrhman28807@gmail.com>
2026-05-25 09:56:06 +0300

    Update GameObject.h

--- Core/GameObject.h
+++ Core/GameObject.h
@@ -1,25 +1,147 @@
 #pragma once
-#include "Drawable.h"
-class GameObject : public Drawable
+#include "../CMUgraphicsLib/CMUgraphics.h"
+#include "../UI/Toolbar.h"
+#include "../UI/BudgetBar.h"
+#include <vector>
+
+class Animal;
+
+enum AnimalType
 {
-protected:
-	color fillColor;	//game object fill color
-	color borderColor;	//game object border color
+	ANIMAL_CHICK,
+	ANIMAL_COW
+};
+
+class Game
+{
+private:
+	window* pWind;
+	Toolbar* gameToolbar;
+	Budgetbar* gameBudgetbar;
+	std::vector<point> wolvesVel;
+	std::vector<Animal*> animals;
+	std::vector<point> wolves;
+	std::vector<int> wolfHitCounts;
+
+	struct GrassData
+	{
+		point pos;
+		int timeRemaining;
+	};
+
+	std::vector<GrassData> grassPatches;
+
+	struct ProductData
+	{
+		string label;
+		point pos;
+	};
+
+	std::vector<ProductData> fieldProducts;
+	
+	bool paused;
+	int eggCount;
+	int milkCount;
+
+	int warehouseEggCount;
+	int warehouseMilkCount;
+	int goalTarget;
+	int goalProgress;
+	bool mainWolfVisible;
+	int consecutiveWolfClicks;
+	mutable string statusMessage;
+	mutable int statusMessageTimer;
+
+	bool wolvesSpawned;
+	int wolfSpawnCountdown;
+	
+	struct DeadWolf
+	{
+		bool isMain;
+		int respawnCountdown;
+	};
+	std::vector<DeadWolf> deadWolves;
+	void tickWolfRespawns();
+
+	bool isAreaOccupiedByAnimal(int x, int y, int width, int height) const;
+	void clearPlayingArea() const;
+	void drawFieldBackground() const;
+	void drawWolf() const;
+	void drawWarehouse() const;
+	void drawFieldProducts() const;
+	void drawCollectAllButton() const;
+	void addFieldProduct(const string& productLabel, point animalPosition, int animalWidth, int animalHeight);
+	point getRandomAnimalPosition(int animalWidth, int animalHeight) const;
+	bool collectFieldProductAt(int x, int y);
+	bool isPointInsideCollectAllButton(int x, int y) const;
+	void sellEggProducts(int amount);
+	void sellMilkProducts(int amount);
+	bool isPointInsidePrimaryWolf(int x, int y) const;
+	bool isPointInsideExtraWolf(int index, int x, int y) const;
+	bool isPointInsideWarehouse(int x, int y) const;
+	bool isPointInsideEggSellButton(int x, int y) const;
+	bool isPointInsideMilkSellButton(int x, int y) const;
+	void showWarehouseWindow();
+	void handlePlayAreaClick(int x, int y);
+	void checkLevelGoal();
+	void advanceLevel();
+	void resetWolfHitCounters(int clickedWolfIndex);
+	void updateStatusMessageTimer();
 
 public:
-	GameObject(Game* r_pGame, point ref, int r_width, int r_height, color fc, color bc);
-	virtual void draw() const = 0; //for a game object to draw itself on the screen
-	void setRefPoint(point p);
-	bool CollisionDetection(const GameObject& gObj);
+	int budget = 2500;
+	int level = 1;
+	int time = 150;
+	int animalcount = 0;
 
-	//-- The following functions should be supported by the shape class
-	//-- It should be overridden by each inherited shape
-	//-- Decide the parameters that you should pass to each function	
+	mutable int wolfX;
+	mutable int wolfY;
+	mutable int wolfVelX;
+	mutable int wolfVelY;
+	void moveWolf();
 
-	//virtual void move() = 0;		//Move the game object
-	//virtual void save(ofstream &OutFile) = 0;	//Save the shape parameters to the file
-	//virtual void load(ifstream &Infile) = 0;	//Load the shape parameters to the file
-	//virtual void collisionAction() = 0;    //action that should be performed upon collision
+	void addGrassPatch(point p);
+	void drawGrass() const;
+	bool isStandingOnGrass(point p, int w, int h) const;
 
+	void collectEggs();
+	void collectMilk();
+
+	void gametimer(int level);
+	Game();
+	~Game();
+
+	void updatePlayArea();
+	void updateAnimalProduction(int elapsedSeconds);
+
+	clicktype getMouseClick(int& x, int& y) const;
+	string getSrting() const;
+	window* CreateWind(int, int, int, int) const;
+	void createToolbar();
+	void createBudgetbar();
+	void clearBudget() const;
+	void printBudget(string msg) const;
+	void clearStatusBar() const;
+	void updatestatusbar() const;
+	void drawfieldboundary() const;
+	void Restart();
+	void printMessage(string msg) const;
+	void spawnWolves();
+	void go();
+	void redrawField() const;
+	bool canAfford(int amount) const;
+	bool spendBudget(int amount);
+	void placeAnimal(AnimalType animalType);
+	void placeFoodArea();
+	void pauseGame();
+	void resumeGame();
+	void saveGame();
+	void loadGame();
+	void collectAllFieldProducts();
+	void sellEggProducts();
+	void sellMilkProducts();
+	bool isPaused() const;
+	void registerAnimalProduct(const string& productLabel);
+	window* getWind() const;
 };
 
